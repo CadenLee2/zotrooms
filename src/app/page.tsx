f82736e854db99e1be4b6a414ad7dc3676d39425
Caden Lee <maltyg3332@gmail.com>
2026-02-22 11:56:42 -0800

    refactor: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,31 +2,12 @@
 
 import "./page.css";
 
-import Header from '../components/Header/Header';
-import StudyRoomList from '../components/StudyRoomList/StudyRoomList';
-import RatingModal from '../components/RatingModal/RatingModal';
-
-import { StudyRoom } from '../types/types';
-
-import { useSelectedRoomId } from '../helpers/hooks';
-import { getRated } from '../helpers/mockApi';
-
-import { useState, useEffect } from 'react';
-
 import MainPage from '../components/MainPage/MainPage';
 
 import { store } from '../store/store';
 import { Provider } from 'react-redux';
 
 export default function Home() {
-  const { selectedRoomId } = useSelectedRoomId();
-
-  const [rated, setRated] = useState<StudyRoom[]>([]);
-
-  useEffect(() => {
-    getRated().then(res => setRated(res));
-  });
-
   return (
     <Provider store={store}>
       <MainPage />
