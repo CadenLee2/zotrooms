531261345f5fefb99ff2796add10c7f609250344
Dante Dam <laggycomputer@yahoo.com>
2026-02-22 17:19:26 -0800

    do setup immediately

--- src/app/api/lib.ts
+++ src/app/api/lib.ts
@@ -1,6 +1,17 @@
 import { Pool } from "pg";
 
-export const pool = new Pool({
+const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     max: 5,
 });
+
+await pool.query(`
+create table if not exists review (
+    id uuid primary key not null default gen_random_uuid(),
+    room_id varchar unique not null,
+    rating int not null check (rating >= 1 and rating <= 5),
+    explanation varchar not null
+);
+`);
+
+export { pool };
--- src/app/api/setup.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists review (
-    id uuid primary key not null default gen_random_uuid(),
-    room_id varchar unique not null,
-    rating int not null check (rating >= 1 and rating <= 5),
-    explanation varchar not null
-);
-
