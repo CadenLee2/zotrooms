3f2268d6c7dcd2b8a3f96b8340ff36bb6c8d5e61
Caden Lee <maltyg3332@gmail.com>
2026-02-23 19:50:51 -0800

    feat: use shorter api link

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -62,7 +62,7 @@ export default function MainPage() {
       <div className="attribution">
         Built for <a href="https://icssc.club/">ICSSC</a>&apos;s IrvineHacks 2026 workshop{' '}
         • <a href="https://github.com/CadenLee2/zotrooms">Source</a>{' '}
-        • Data from <a href="https://docs.icssc.club/docs/about/anteaterapi">Anteater API</a>
+        • Data from <a href="https://icssc.link/about-anteaterapi">Anteater API</a>
       </div>
       {selectedRoomId && <RatingModal />}
     </div>
