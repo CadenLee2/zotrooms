9ffa17ce788b5a466b2fc4010d5c7595dc75b00a
Dante Dam <laggycomputer@yahoo.com>
2026-02-24 10:49:44 -0800

    yeah no let's just not do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,11 +13,6 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
-  {
-    rules: {
-      quotes: ["error", "double"],
-    },
-  },
 ]);
 
 export default eslintConfig;
