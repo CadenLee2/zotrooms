526c9fbc3a1fe2a8184340409d45e825a5d0da53
Caden Lee <maltyg3332@gmail.com>
2026-02-24 14:19:21 -0800

    fix: build error with missing suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,14 @@ import MainPage from "../components/MainPage/MainPage";
 import { store } from "@/store/store";
 import { Provider } from "react-redux";
 
+import { Suspense } from "react";
+
 export default function Home() {
   return (
-    <Provider store={store}>
-      <MainPage />
-    </Provider>
+    <Suspense fallback={<span>Loading...</span>}>
+      <Provider store={store}>
+        <MainPage />
+      </Provider>
+    </Suspense>
   );
 }
